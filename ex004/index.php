724034e05b83c23af97180200a4f1ed32a543c57
Samuel Abreu <samuelabreudev@gmail.com>
2026-03-05 15:27:55 -0300

    feat: ex004 (Manipulaçao de Strings em PHP)

---
+++ ex004/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strings em PHP</title>
+</head>
+<body>
+    <h1>Manipulação de Strings em PHP</h1>
+    <?php 
+        // FORMATOS DE STRINGS
+        // $double_quoted = "Curso" . "PHP"; // O (.) e um operador de concatenaçao 
+        // $single_quoted = 'Curso';
+
+        // HEREDOC
+        // $HereDoc = "PHP";
+        // $ano = Date('Y'); 
+         
+        // echo <<< FRASE
+        //     Estou estudando
+        //         $Heredoc em $ano 
+        
+        // FRASE;
+
+        // NOWDOC
+        // $NowDoc = "PHP";
+        // $ano = Date('Y'); 
+         
+        // echo <<< FRASE
+        //     Estou estudando
+        //         $Heredoc em $ano 
+        
+        // FRASE;
+
+    
+    ?>
+    
+</body>
+</html>
